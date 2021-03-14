73a7be2385b62298424966d70820c4e246f70236
Nicco <nicolaugalves@gmail.com>
2021-03-14 10:57:04 -0300

    last config to work on AWS

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^$^d_e5h*+!cxxe^*-c=$m2bnlwfcrnjf)t)pj3xid9&6#f3+v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -120,3 +120,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
